86bb40213da6baf41102aff4e4be19ba6db10c8b
NakulMishra <nakulmishra746@gmail.com>
2026-06-22 22:55:22 +0530

    Created and implemented Service Layer with validation along with Exceptions

---
+++ backend/src/main/java/com/application/placementmanagementsystem/company/service/CompanyServiceImpl.java
@@ -0,0 +1,86 @@
+package com.application.placementmanagementsystem.company.service;
+
+import com.application.placementmanagementsystem.company.dtos.CompanyCreateRequest;
+import com.application.placementmanagementsystem.company.dtos.CompanyResponse;
+import com.application.placementmanagementsystem.company.dtos.CompanyUpdateRequest;
+import com.application.placementmanagementsystem.exceptions.DuplicateResourceException;
+import com.application.placementmanagementsystem.exceptions.ResourceNotFoundException;
+import com.application.placementmanagementsystem.company.mapper.CompanyMapper;
+import com.application.placementmanagementsystem.models.Company;
+import com.application.placementmanagementsystem.repositories.CompanyRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+    private final CompanyMapper companyMapper;
+
+    @Override
+    public CompanyResponse createCompany(
+            CompanyCreateRequest request) {
+
+        if (companyRepository.existsByCompanyNameIgnoreCase(
+                request.getCompanyName())) {
+
+            throw new DuplicateResourceException(
+                    "Company already exists");
+        }
+
+        Company company = companyMapper.toEntity(request);
+
+        Company savedCompany =
+                companyRepository.save(company);
+
+        return companyMapper.toResponse(savedCompany);
+    }
+
+    @Override
+    public CompanyResponse updateCompany(
+            Long id,
+            CompanyUpdateRequest request) {
+
+        Company company = companyRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Company not found with id " + id));
+
+        company.setCompanyName(request.getCompanyName());
+        company.setIndustry(request.getIndustry());
+        company.setWebsite(request.getWebsite());
+        company.setLocation(request.getLocation());
+        company.setDescription(request.getDescription());
+
+        Company updatedCompany =
+                companyRepository.save(company);
+
+        return companyMapper.toResponse(updatedCompany);
+    }
+
+    @Override
+    public CompanyResponse getCompanyById(Long id) {
+
+        Company company = companyRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Company not found with id " + id));
+
+        return companyMapper.toResponse(company);
+    }
+
+    @Override
+    public List<CompanyResponse> getAllCompanies() {
+
+        return companyRepository.findAll()
+                .stream()
+                .map(companyMapper::toResponse)
+                .toList();
+    }
+
+}
