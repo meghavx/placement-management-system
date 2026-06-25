c30120a0121cd9ce6b1dfbaf05dbb2d968124128
NakulMishra <nakulmishra746@gmail.com>
2026-06-25 10:23:24 +0530

    Added company module service

---
+++ backend/src/main/java/com/application/placementmanagementsystem/services/company/CompanyService.java
@@ -0,0 +1,18 @@
+package com.application.placementmanagementsystem.services.company;
+
+import com.application.placementmanagementsystem.dtos.company.CompanyCreateRequest;
+import com.application.placementmanagementsystem.dtos.company.CompanyResponse;
+import com.application.placementmanagementsystem.dtos.company.CompanyUpdateRequest;
+
+import java.util.List;
+
+public interface CompanyService {
+
+    CompanyResponse createCompany(CompanyCreateRequest request);
+
+    CompanyResponse updateCompany(Long id, CompanyUpdateRequest request);
+
+    CompanyResponse getCompanyById(Long id);
+
+    List<CompanyResponse> getAllCompanies();
+}
