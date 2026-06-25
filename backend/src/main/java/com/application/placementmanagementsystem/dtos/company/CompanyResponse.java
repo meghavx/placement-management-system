32c9d8ba053c64a32bef05c608e339869f4e95e0
NakulMishra <nakulmishra746@gmail.com>
2026-06-25 10:16:08 +0530

    Added company module DTOs and mapper

---
+++ backend/src/main/java/com/application/placementmanagementsystem/dtos/company/CompanyResponse.java
@@ -0,0 +1,21 @@
+package com.application.placementmanagementsystem.dtos.company;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CompanyResponse {
+
+    private Long id;
+
+    private String companyName;
+
+    private String industry;
+
+    private String website;
+
+    private String location;
+
+    private String description;
+}
