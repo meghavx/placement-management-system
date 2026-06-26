a8e6810dfce784f23b410c1a60f31923f7a006e4
Megha <meghavx@outlook.com>
2026-06-26 20:47:20 +0530

    Initialize super admin

--- backend/src/main/java/com/application/placementmanagementsystem/repositories/UserRepository.java
+++ backend/src/main/java/com/application/placementmanagementsystem/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.application.placementmanagementsystem.repositories;
 
 import com.application.placementmanagementsystem.models.User;
+import com.application.placementmanagementsystem.models.enums.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    boolean existsByRole(RoleType role);
 }
