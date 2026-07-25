d4717da3b7ae24cf343c8c790b8199292c8e6300
Megha <meghavx@outlook.com>
2026-07-26 03:17:00 +0530

    Implemented profile management module

--- backend/src/main/java/com/application/placementmanagementsystem/repositories/StudentRepository.java
+++ backend/src/main/java/com/application/placementmanagementsystem/repositories/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.application.placementmanagementsystem.repositories;
 
 import com.application.placementmanagementsystem.models.Student;
+import com.application.placementmanagementsystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +10,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     boolean existsByRollNumber(String rollNumber);
 
     Optional<Student> findByRollNumber(String rollNumber);
+
+    Optional<Student> findByUser(User user);
 }
