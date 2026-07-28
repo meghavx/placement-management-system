3a68980b90d0510b3b8e8ca1ffd18dd796f7db63
Nakul Mishra <nakulmishra746@gmail.com>
2026-07-28 12:02:49 +0530

    Modified Repository and Added Service Interface

--- backend/src/main/java/com/application/placementmanagementsystem/repositories/NotificationRepository.java
+++ backend/src/main/java/com/application/placementmanagementsystem/repositories/NotificationRepository.java
@@ -1,7 +1,17 @@
 package com.application.placementmanagementsystem.repositories;
 
 import com.application.placementmanagementsystem.models.Notification;
+import com.application.placementmanagementsystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+
+    Optional<Notification> findByIdAndUser(
+            Long id,
+            User user
+    );
 }
