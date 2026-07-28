05ef1a8f00955bfc72746bab09f746c64f79aa3e
Nakul Mishra <nakulmishra746@gmail.com>
2026-07-29 02:02:23 +0530

    Added Swagger Annotations

--- backend/src/main/java/com/application/placementmanagementsystem/controllers/AuditLogController.java
+++ backend/src/main/java/com/application/placementmanagementsystem/controllers/AuditLogController.java
@@ -4,6 +4,8 @@ import com.application.placementmanagementsystem.common.ResponseBuilder;
 import com.application.placementmanagementsystem.dtos.audit.AuditLogResponse;
 import com.application.placementmanagementsystem.common.ApiResponse;
 import com.application.placementmanagementsystem.services.audit.AuditLogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/audit-logs")
+@Tag(name = "Audit Logs")
 public class AuditLogController {
 
     private final AuditLogService auditLogService;
 
     @GetMapping
+    @Operation(summary = "Get Audit Logs")
     public ApiResponse<List<AuditLogResponse>> getAuditLogs() {
 
         return ResponseBuilder.success(
