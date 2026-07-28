f1020266fa2d0ac15b7d2e233a3e8c9be3bd3108
Megha <meghavx@outlook.com>
2026-07-28 23:29:24 +0530

    Added email setup

---
+++ backend/src/main/java/com/application/placementmanagementsystem/services/email/EmailService.java
@@ -0,0 +1,26 @@
+package com.application.placementmanagementsystem.services.email;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${app.mail.from}")
+    private String fromEmail;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
