2ca77c90aea25f72ed4f6bcfd4a613f93cd452b3
Megha <meghavx@outlook.com>
2026-07-29 05:10:49 +0530

    Added recruitment activity entity and repository

---
+++ backend/src/main/java/com/application/placementmanagementsystem/models/enums/RecruitmentActivityType.java
@@ -0,0 +1,12 @@
+package com.application.placementmanagementsystem.models.enums;
+
+public enum RecruitmentActivityType {
+    APTITUDE_TEST,
+    CODING_TEST,
+    TECHNICAL_TEST,
+    GROUP_DISCUSSION,
+    TECHNICAL_INTERVIEW,
+    HR_INTERVIEW,
+    MANAGERIAL_INTERVIEW,
+    OTHER
+}
