71e2a4df7d98c6b67d9de3d00c955ce26b1b2c27
Nakul Mishra <nakulmishra746@gmail.com>
2026-08-01 02:42:25 +0530

    Added ReportMapper and Modified ApplicationRepository

--- backend/src/main/java/com/application/placementmanagementsystem/repositories/ApplicationRepository.java
+++ backend/src/main/java/com/application/placementmanagementsystem/repositories/ApplicationRepository.java
@@ -4,6 +4,8 @@ import com.application.placementmanagementsystem.models.Application;
 import com.application.placementmanagementsystem.models.PlacementDrive;
 import com.application.placementmanagementsystem.models.Student;
 import com.application.placementmanagementsystem.models.enums.ApplicationStatus;
+import com.application.placementmanagementsystem.models.enums.Department;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -42,4 +44,89 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
     );
 
     long countByStatus(ApplicationStatus status);
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByStatus(ApplicationStatus status);
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByStudentDepartment(Department department);
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByPlacementDriveCompanyId(Long companyId);
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByStudentId(Long studentId);
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByStudentGraduationYear(Integer graduationYear);
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByStudentDepartmentAndStudentGraduationYear(
+            Department department,
+            Integer graduationYear
+    );
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByPlacementDriveCompanyIdAndStudentGraduationYear(
+            Long companyId,
+            Integer graduationYear
+    );
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByPlacementDriveCompanyIdAndStudentDepartment(
+            Long companyId,
+            Department department
+    );
+
+    @EntityGraph(attributePaths = {
+            "student",
+            "student.user",
+            "placementDrive",
+            "placementDrive.company"
+    })
+    List<Application> findByPlacementDriveCompanyIdAndStudentDepartmentAndStudentGraduationYear(
+            Long companyId,
+            Department department,
+            Integer graduationYear
+    );
 }
